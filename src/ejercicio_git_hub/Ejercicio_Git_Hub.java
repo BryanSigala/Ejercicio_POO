c4a7b1ade7afbe6fb01f359728ddcb1ca746504e
Envy <Envy@DESKTOP-7I42Q68>
2021-04-19 18:54:55 -0600

    Programa despliega saludos, primer ejercicio

---
+++ src/ejercicio_git_hub/Ejercicio_Git_Hub.java
@@ -0,0 +1,16 @@
+
+package ejercicio_git_hub;
+
+
+public class Ejercicio_Git_Hub {
+
+    
+    public static void main(String[] args) {
+        
+        String saludo = "Hola mundo, mi nombre es ";
+        String nombre = "Bryan Aaron Sigala Jaime";
+        System.out.println(saludo + nombre + "\n"
+                + "Iniciales: " + nombre.charAt(0) + nombre.charAt(6) + nombre.charAt(12) + nombre.charAt(19));
+    }
+    
+}
