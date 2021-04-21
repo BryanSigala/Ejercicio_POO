083bbbb6efe32174155fc5e23043527406ed5d6c
Envy <Envy@DESKTOP-7I42Q68>
2021-04-21 12:34:35 -0600

    HOla

--- src/ejercicio_git_hub/Ejercicio_Git_Hub.java
+++ src/ejercicio_git_hub/Ejercicio_Git_Hub.java
@@ -10,7 +10,9 @@ public class Ejercicio_Git_Hub {
         String saludo = "Hola mundo, mi nombre es ";
         String nombre = "Bryan Aaron Sigala Jaime";
         System.out.println(saludo + nombre + "\n"
-                + "Iniciales: " + nombre.charAt(0) + nombre.charAt(6) + nombre.charAt(12) + nombre.charAt(19));
+                + "Iniciales: " + nombre.charAt(0) + nombre.charAt(6) 
+                + nombre.charAt(12) + nombre.charAt(19));
+        System.out.println("Holaaaaa");
     }
     
 }
